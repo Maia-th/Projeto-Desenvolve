16f059c77eb1ee36ca7355df38bc433db0335ecd
Maia-th <thiagomaia633@gmail.com>
2024-06-21 16:38:50 -0300

    6.4 - Compreensão de listas

---
+++ desenvolve-python-basico/modulo6/aula4_questao1.py
@@ -0,0 +1,11 @@
+pares = [num for num in range(20, 51) if num % 2 == 0]
+print("Pares entre 20 e 50:", pares)
+
+quadrados = [num**2 for num in range(1, 10)]
+print("Quadrados dos valores de 1 a 9:", quadrados)
+
+divisiveis_por_7 = [num for num in range(1, 101) if num % 7 == 0]
+print("Divisíveis por 7 entre 1 e 100:", divisiveis_por_7)
+
+paridade = ["par" if num % 2 == 0 else "ímpar" for num in range(0, 30, 3)]
+print("Paridade dos valores em range(0,30,3):", paridade)
