51dc702789ccb2317df8857d3297f238e53b52de
Maia.th <thiagomaia633@gmail.com>
2024-10-25 22:31:54 -0300

    atividade implementacao

---
+++ desenvolve-poo/modulo2/Autor.java
@@ -0,0 +1,32 @@
+public class Autor extends Pessoa {
+    private String nacionalidade;
+
+    public Autor(String nome, Livro[] livros, String nacionalidade) {
+        super(nome, livros);
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public Livro[] getObrasPublicadas() {
+        return livros;
+    }
+
+    public void setLivros(Livro[] livros) {
+        this.livros = livros;
+    }
+
+    public Livro[] getObrasPublicadasPorGeneros(String genero) {
+        Livro[] obrasPorGenero = new Livro[livros.length];
+        int count = 0;
+        for (Livro livro : livros) {
+            if (livro.getGenero().equals(genero)) {
+                obrasPorGenero[count] = livro;
+                count++;
+            }
+        }
+        return obrasPorGenero;
+    }
+}
---
+++ desenvolve-poo/modulo2/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario extends Pessoa {
+    private int idade;
+    private Emprestimo[] historicoEmprestimos;
+
+    public Usuario(String nome, Livro[] livros, int idade, Emprestimo[] historicoEmprestimos) {
+        super(nome, livros);
+        this.idade = idade;
+        this.historicoEmprestimos = historicoEmprestimos;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Emprestimo[] getHistoricoEmprestimos() {
+        return historicoEmprestimos;
+    }
+
+    public void setHistoricoEmprestimos(Emprestimo[] historicoEmprestimos) {
+        this.historicoEmprestimos = historicoEmprestimos;
+    }
+}
